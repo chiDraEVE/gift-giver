6151999d8c46057ca3427f35b191b4c02cdfb8df
chidraeve <chidraeve@clubitsolutions.de>
2019-07-29 15:04:02 +0200

    16. Remove a Gift

--- gift-giver/src/components/Gift.js
+++ gift-giver/src/components/Gift.js
@@ -15,17 +15,21 @@ class Gift extends React.Component {
                         <FormLabel>Person</FormLabel>
                         <FormControl
                             className={'input-person'}
-                            onChange={event=> this.setState({person: event.target.value})}
+                            onChange={event => this.setState({person: event.target.value})}
                         />
                     </FormGroup>
                     <FormGroup>
                         <FormLabel>Present</FormLabel>
                         <FormControl
                             className={'input-present'}
-                            onChange={event=> this.setState({present: event.target.value})}
+                            onChange={event => this.setState({present: event.target.value})}
                         />
                     </FormGroup>
                 </Form>
+                <Button
+                    className={'btn-remove'}
+                    onClick={() => this.props.removeGift(this.props.gift.id)}
+                >Remove Gift</Button>
             </div>
         );
     }
